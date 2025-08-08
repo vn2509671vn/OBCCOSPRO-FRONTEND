f26bd1d8764866255c6906074d93be33f6394f8b
ThangTGM <vn2509671vn@gmail.com>
2025-08-08 10:39:59 +0700

    change ngrok

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //localStorage.setItem("http_endpoint", "https://a163-113-161-213-71.ngrok-free.app/"); // thangtgm93@gmail.com
 //localStorage.setItem("http_endpoint", "https://b33f-113-161-213-71.ngrok-free.app/"); // vn2509671vn@gmail.com
 //localStorage.setItem("http_endpoint", "https://92f5-113-161-213-71.ngrok-free.app/"); // vn2509671vn2@gmail.com
-localStorage.setItem("http_endpoint", "https://66766d2f0d4a.ngrok-free.app/"); // vn25096711@gmail.com
+localStorage.setItem("http_endpoint", "https://3f3de2bef1af.ngrok-free.app/"); // vn25096711@gmail.com
 localStorage.setItem("http_local_endpoint", "http://10.94.35.204:9000/");
 
 const today = new Date();
